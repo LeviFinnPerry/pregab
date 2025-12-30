03c588068f746092b963d92f92867813557810b0
Perry, Levi <jp247@students.waikato.ac.nz>
2025-12-30 18:43:23 +1300

    Split study overview into a csv file

--- processing.py
+++ processing.py
@@ -1,1 +1,18 @@
 # Imports
+import json
+import pandas as pd
+
+# Load JSON data from a file
+with open('NCT00830167.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    
+# 1. Study overview
+study_info = {
+    'nct_id': data['protocolSection']['identificationModule']['nctId'],
+    'brief_title': data['protocolSection']['identificationModule']['briefTitle'],
+    'enrollment': data['protocolSection']['designModule']['enrollmentInfo']['count'],
+    'phase': data['protocolSection']['designModule']['phases'][0],
+    'arms': [arm['label'] for arm in data['protocolSection']['armsInterventionsModule']['armGroups']]
+}
+study_df = pd.DataFrame([study_info])
+study_df.to_csv('study_overview.csv', index=False)
